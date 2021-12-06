11572bef76ae56015e5b4d82fe47fecc9b27e238
teguhdwic15 <dwiteguh4@gmail.com>
2021-12-06 17:01:37 +0700

    initial commit

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import os
 import uuid
 import flask
 import urllib
-import tensorflow as tf
 from PIL import Image
 from tensorflow.keras.models import load_model
 from flask import Flask , render_template  , request , send_file
